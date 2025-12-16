c0bd4b6b4142345314a37a0da858f59d64578f4b
mogus-kid <xmccub@bayhaven.org>
2025-12-16 13:53:33 -0600

    Delete helloworld.java

--- helloworld.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String hello_variable1 = "Hello ";
-        String language_name = "Java ";
-        String hello_variable2 = "World!";
-
-        System.out.println(hello_variable1 + language_name + hello_variable2);
-    }
-}
